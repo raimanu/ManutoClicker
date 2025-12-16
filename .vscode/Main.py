d5d0559dfb135f791057d58ceb875bb907865bd2
Raimanu Pain <68045625+raimanu@users.noreply.github.com>
2025-12-16 23:54:12 +0000

    Premier code de test

--- .vscode/Main.py
+++ .vscode/Main.py
@@ -1,12 +1,3 @@
 import keyboard
 
-def main():
-    print("Press 'q' to quit.")
-    while True:
-        if keyboard.is_pressed('q'):
-            break
-        if keyboard.is_pressed('space'):
-            print("Spacebar pressed!")
-
-if __name__ == "__main__":
-    main()
+keyboard.press_and_release('space')
